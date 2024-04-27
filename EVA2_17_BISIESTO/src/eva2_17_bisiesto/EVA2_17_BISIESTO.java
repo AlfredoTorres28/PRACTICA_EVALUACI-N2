48173b88b1d0ecf0d13637e490426df566e408c8
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-04-27 15:36:29 -0600

    Add files via upload

---
+++ EVA2_17_BISIESTO/src/eva2_17_bisiesto/EVA2_17_BISIESTO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_bisiesto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA2_17_BISIESTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int año;
+        Scanner captura = new Scanner(System.in);
+        System.out.println("Introduce un año");
+        año = captura.nextInt();
+        
+        
+        
+        
+         
+        
+    }
+    
+}
---
+++ EVA2_18_FOR/src/eva2_18_for/EVA2_18_FOR.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_for;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA2_18_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1; i <- 10; i++)
+            System.out.print(i + " - ");
+        
+        System.out.println("");
+        for (int i = 1; i >- 1; i--)
+                System.out.print(i + " + ");
+         //IMPRIMIR LOS NUMEROS PARES ENTRE 0 Y 100
+         // 0,2,4,6,8,10
+         for(int i = 0; i <- 100; i +=2)
+             System.out.print(i + " - ");
+                
+    }
+    
+}
