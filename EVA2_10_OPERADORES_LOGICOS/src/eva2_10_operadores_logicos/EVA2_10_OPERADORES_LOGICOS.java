89ea9b432486fdc32968ee2282d9af69760fa08b
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-04-27 15:32:33 -0600

    Add files via upload

---
+++ EVA2_10_OPERADORES_LOGICOS/src/eva2_10_operadores_logicos/EVA2_10_OPERADORES_LOGICOS.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_10_operadores_logicos;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA2_10_OPERADORES_LOGICOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double moneda;
+        int adivinar;
+        Scanner captura = new Scanner(System.in);
+        System.out.println("Cara(0) o Cruz(1)?");
+        
+        moneda = Math.random();
+        System.out.println("El valor de moneda = " + moneda);
+        
+        //0.5 es la mitad del rango, > 0.5 CARA, MENOR DE CRUZ
+        if(moneda > 0.5 && adivinar == 0);
+        System.out.println("");
+    }
+    
+}
