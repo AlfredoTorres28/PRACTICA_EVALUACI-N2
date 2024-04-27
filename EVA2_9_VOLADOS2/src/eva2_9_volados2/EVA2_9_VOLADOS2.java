8ee355cc1edead031a36056c52cccd059c7293f9
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-04-27 15:32:12 -0600

    Add files via upload

---
+++ EVA2_9_VOLADOS2/src/eva2_9_volados2/EVA2_9_VOLADOS2.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_9_volados2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA2_9_VOLADOS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double moneda;
+       moneda = Math.random();
+       System.out.println("Adivina el lado de la moneda");
+       Scanner captura = new Scanner(System.in);
+       captura.nextLine();
+       if(moneda > 0.5)
+              System.out.println("Ganaste");
+        else
+           System.out.println("Intentalo de nuevo");
+           
+       
+       {
+          
+           
+           
+
+}
+    
+            
+    
+    }
+    
+}
